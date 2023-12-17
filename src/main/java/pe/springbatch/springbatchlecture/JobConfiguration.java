28afb86c9dca034258e8a4ae8974d3bd9a93b19a
Denny-Ko <donghar@naver.com>
2023-12-17 13:59:55 +0900

    JobBuilder

--- src/main/java/pe/springbatch/springbatchlecture/JobConfiguration.java
+++ src/main/java/pe/springbatch/springbatchlecture/JobConfiguration.java
@@ -4,7 +4,9 @@ package pe.springbatch.springbatchlecture;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
@@ -16,11 +18,20 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class JobConfiguration {
+//	@Bean
+//	public Job batchJob1(JobRepository jobRepository, PlatformTransactionManager txManager) throws Exception {
+//		return new JobBuilder("Job1", jobRepository)
+//			.start(step1(jobRepository, txManager))
+//			.next(step2(jobRepository, txManager))
+//			.build();
+//	}
+
 	@Bean
-	public Job batchJob1(JobRepository jobRepository, PlatformTransactionManager txManager) throws Exception {
-		return new JobBuilder("Job1", jobRepository)
-			.start(step1(jobRepository, txManager))
-			.next(step2(jobRepository, txManager))
+	public Job batchJob2(JobRepository jobRepository, PlatformTransactionManager txManager) throws Exception {
+		return new JobBuilder("Job2", jobRepository)
+			.start(flow(jobRepository, txManager))
+			.next(step5(jobRepository, txManager))
+			.end()
 			.build();
 	}
 
@@ -48,5 +59,53 @@ public class JobConfiguration {
 				}
 			}, txManager)
 			.build();
+	}
+
+	@Bean
+	public Step step3(JobRepository jobRepository, PlatformTransactionManager txManager) {
+		return new StepBuilder("step3", jobRepository)
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+					System.out.println("step3 has executed");
+					return RepeatStatus.FINISHED;
+				}
+			}, txManager)
+			.build();
+	}
+
+	@Bean
+	public Step step4(JobRepository jobRepository, PlatformTransactionManager txManager) {
+		return new StepBuilder("step4", jobRepository)
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+					System.out.println("step4 has executed");
+					return RepeatStatus.FINISHED;
+				}
+			}, txManager)
+			.build();
+	}
+
+	@Bean
+	public Step step5(JobRepository jobRepository, PlatformTransactionManager txManager) {
+		return new StepBuilder("step5", jobRepository)
+			.tasklet(new Tasklet() {
+				@Override
+				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+					System.out.println("step5 has executed");
+					return RepeatStatus.FINISHED;
+				}
+			}, txManager)
+			.build();
+	}
+
+	@Bean
+	public Flow flow(JobRepository jobRepository, PlatformTransactionManager txManager) {
+		FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+		return flowBuilder
+			.start(step3(jobRepository, txManager))
+			.next(step4(jobRepository, txManager))
+			.end();
 	}
 }
