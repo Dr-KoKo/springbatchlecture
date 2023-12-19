44b83e3dd10a5a6cae98f6d014b93e34ae04c1ef
Denny-Ko <donghar@naver.com>
2023-12-19 15:27:38 +0900

    SimpleJob 아키텍처

--- src/main/java/pe/springbatch/springbatchlecture/SimpleJobArchitectureConfiguration.java
+++ src/main/java/pe/springbatch/springbatchlecture/SimpleJobArchitectureConfiguration.java
@@ -2,26 +2,22 @@ package pe.springbatch.springbatchlecture;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
-public class IncrementerConfiguration {
+public class SimpleJobArchitectureConfiguration {
 	@Bean
 	public Job batchJob(JobRepository jobRepository, PlatformTransactionManager txManager) {
 		return new JobBuilder("Job", jobRepository)
 			.start(step1(jobRepository, txManager))
 			.next(step2(jobRepository, txManager))
-			//			.incrementer(new CustomJobParametersIncrementer())
 			.incrementer(new RunIdIncrementer())
 			.build();
 	}
@@ -29,12 +25,9 @@ public class IncrementerConfiguration {
 	@Bean
 	public Step step1(JobRepository jobRepository, PlatformTransactionManager txManager) {
 		return new StepBuilder("step1", jobRepository)
-			.tasklet(new Tasklet() {
-				@Override
-				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-					System.out.println("step1 has executed");
-					return RepeatStatus.FINISHED;
-				}
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step1 has executed");
+				return RepeatStatus.FINISHED;
 			}, txManager)
 			.build();
 	}
@@ -42,12 +35,9 @@ public class IncrementerConfiguration {
 	@Bean
 	public Step step2(JobRepository jobRepository, PlatformTransactionManager txManager) {
 		return new StepBuilder("step2", jobRepository)
-			.tasklet(new Tasklet() {
-				@Override
-				public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-					System.out.println("step2 has executed");
-					return RepeatStatus.FINISHED;
-				}
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("step2 has executed");
+				return RepeatStatus.FINISHED;
 			}, txManager)
 			.build();
 	}
